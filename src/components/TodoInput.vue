9fdddc402d746232b92955833b1447279f80c87c
devmin24 <pmk4236@naver.com>
2021-09-12 03:23:53 +0900

    todoList > check button, delete button implement

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <!-- v-on:keyup.enter : enter 입력하면 "" 실행하기 -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer" v-on:click="addTodo">
+      <!-- awesome icon 가져와서 쓰기, css도 직접 적용할 수 있음 -->
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newTodoItem !== "") {
+        var obj = { completed: false, item: this.newTodoItem };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj)); // 객체를 가져올 때, JSON.stringify를 사용해 문자열로 가져와야함.
+        this.clearInput(); // this로 같은 인스턴스 안에 있는 메소드도 가져올 수 있음.
+      }
+    },
+    clearInput: function() {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+  width: 80%;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,94 @@
 <template>
   <div>
-    list
+    <ul>
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="fas fa-check checkBtn"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+          todoItem.item
+        }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem); // 로컬스토리지에서 지우고,
+      this.todoItems.splice(index, 1); // index값으로 배열에서도 지워주기
+    },
+    toggleComplete: function(todoItem) {
+      todoItem.completed = !todoItem.completed; // true이면 false로, false이면 true로 변경
+      // localStorage에 값이 변경될 때마다 기존 값 지우고, 새로운 값 넣어주기(localStorage에 update api가 없으므로 갱신하는 방법)
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          // this.todoItems.push(localStorage.key(i));
+          // JSON.parse = JSON.stringify로 문자열로 변환했던 것을 다시 객체화 하는 것
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+ul {
+  /* list 번호 스타일 */
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
